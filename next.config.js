a58c48af55a979e283f2e56ff65d60c43c52ad2b
Haseab <hullah3000@gmail.com>
2024-06-18 13:27:51 -0700

    add initial landing page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["pub-7712ec77fabb4a6d996c607b226d98f0.r2.dev"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
